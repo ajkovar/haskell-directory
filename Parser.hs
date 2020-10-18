86db54ac023afd0eb95e354032f36dc19fadbfb3
Alex Kovar <ajkovar@gmail.com>
2020-10-18 16:37:32 -0500

    Added line parsing

--- Parser.hs
+++ Parser.hs
@@ -1,14 +1,29 @@
-module Parser (detectSeparator) where
+module Parser (detectSeparator, parseLine, Person(Person)) where
 
 import Data.List (elemIndex)
+import Data.List.Split (splitOn)
+
+data Person = Person { firstName :: String  
+                     , lastName :: String  
+                     , gender :: String  
+                     , favoriteColor :: String  
+                     , dob :: String  
+                     } deriving (Show, Eq) 
 
 contains :: Char -> [Char] -> Bool
 contains char str = case elemIndex char str of
                       Just _ -> True
                       Nothing -> False
 
+detectSeparator :: [Char] -> [Char]
 detectSeparator input 
     | contains ',' input = ","
     | contains '|' input = "|"
     | otherwise = " "
+
+parseArray :: [[Char]] -> Person
+parseArray [first, last, gender, color, dob] = Person first last gender color dob
+
+parseLine :: [Char] -> Person
+parseLine line = parseArray $ splitOn (detectSeparator line) line
 
--- test/Parser/Test.hs
+++ test/Parser/Test.hs
@@ -1,10 +1,10 @@
 module Parser.Test (suite) where
 
 import qualified Test.HUnit as HUnit
-import Parser (detectSeparator)
+import Parser (detectSeparator, parseLine, Person(Person))
 
 suite :: HUnit.Test
-suite = HUnit.TestList [testDetect]
+suite = HUnit.TestList [testDetect, testParseLine]
 
 testDetect :: HUnit.Test
 testDetect = HUnit.TestLabel "detectSeparator" . HUnit.TestCase $ do
@@ -12,3 +12,9 @@ testDetect = HUnit.TestLabel "detectSeparator" . HUnit.TestCase $ do
   HUnit.assertEqual "Detects |" "|" $ detectSeparator "John | Smith"
   HUnit.assertEqual "Detects ' '" " " $ detectSeparator "John Smith"
 
+testParseLine :: HUnit.Test
+testParseLine = HUnit.TestLabel "parseLine" . HUnit.TestCase $ do
+  HUnit.assertEqual "Should read comma separated" person (parseLine "John, Smith, male, red, 04/22/2021")
+    where person = Person "John" "Smith" "male" "red" "04/22/2021"
+
+-- contents <- readFile "test.txt"  
